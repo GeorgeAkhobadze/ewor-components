9111b170502514a3cf5418092d99dd8070714471
axobadzegiorgi5 <axobadzegiorgi5@gmail.com>
2023-06-30 12:42:04 +0400

    responsive changes & bugs

--- modal.js
+++ modal.js
@@ -1,15 +1,55 @@
-var acc = document.getElementsByClassName("modal-dropdown-btn");
-var i;
+let acc = document.getElementsByClassName("modal-dropdown-btn");
+let modalBackground = document.getElementById("modal-bg");
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    console.log("wooh")
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    document.body.style.overflow = "hidden";
+    console.log(document.body);
     this.classList.toggle("modal-dropdown-active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = "120px";
-    } 
+    }
+  });
+
+  if (i === 0) {
+    acc[i].classList.add("modal-dropdown-active");
+    var panel = acc[i].nextElementSibling;
+    panel.style.maxHeight = "120px";
+  }
+}
+
+function hideModalContainer() {
+  var modalBg = document.getElementById("modal-bg");
+  var modalContainer = document.querySelector(".modal-container");
+
+  modalBg.addEventListener("click", function () {
+    modalContainer.style.display = "none";
+    document.body.style.overflow = "initial";
   });
 }
+
+function showModal() {
+  var cardInners = document.getElementsByClassName("card-inner");
+  var modalContainer = document.querySelector(".modal-container");
+  var modalCloseBtn = document.getElementById("modal-close-btn");
+
+  for (var i = 0; i < cardInners.length; i++) {
+    cardInners[i].addEventListener("click", function () {
+      modalContainer.style.display = "flex";
+      document.body.style.overflow = "hidden";
+    });
+  }
+
+  modalCloseBtn.addEventListener("click", function () {
+    modalContainer.style.display = "none";
+    document.body.style.overflow = "initial";
+  });
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  hideModalContainer();
+  showModal();
+});
